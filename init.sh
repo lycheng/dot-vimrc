c44bc4357c06fe5fed7f4123259f170cefa0f974
lycheng <lycheng997@gmail.com>
2013-08-06 11:45:36 +0800

    update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,4 @@
 ln -s ./dot-vimrc/vimrc ~/.vimrc
 ln -s ./dot-vimrc ~/.vim 
 
-sudo apt-get install ack-grep ctags
-# for ubuntu 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-sudo pip install pyflakes
